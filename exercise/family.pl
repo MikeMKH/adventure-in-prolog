436cb5a01458db9bdf34ae3ca84b0d4a2ece73af
Mike Harris <MikeMKH@users.noreply.github.com>
2022-04-05 06:18:11 -0500

    add exercies from chapter 2

---
+++ exercise/family.pl
@@ -0,0 +1,20 @@
+male(jack).
+female(lily).
+
+male(mike).
+female(kelsey).
+
+female(kim).
+
+male(jim).
+female(cindy).
+
+parent(jim, mike).
+parent(jim, kim).
+parent(cindy, mike).
+parent(cindy, kim).
+
+parent(mike, jack).
+parent(mike, lily).
+parent(kelsey, jack).
+parent(kelsey, lily).
