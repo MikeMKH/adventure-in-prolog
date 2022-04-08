88f81a07af357d03f7a025043705b1976aede1bf
Mike Harris <MikeMKH@users.noreply.github.com>
2022-04-08 06:36:09 -0500

    add examples from 5

--- nani_search.pl
+++ nani_search.pl
@@ -27,3 +27,35 @@ poison(cheese).
 turned_off(flashlight).
 
 here(kitchen).
+
+where_food(Food) :-
+  edible(Food),
+  here(Place),
+  location(Food, Place).
+
+connect(Place1, Place2) :- door(Place1, Place2).
+connect(Place1, Place2) :- door(Place2, Place1).
+
+list_things(Place) :-
+  location(Item, Place),
+  tab(2),
+  write(Item),
+  nl,
+  fail.
+list_things(_).
+
+list_connections(Place) :-
+  connect(Place, NextPlace),
+  tab(2),
+  write(NextPlace),
+  nl,
+  fail.
+list_connections(_).
+
+look :-
+  here(Place),
+  write('You are in the '), write(Place), nl,
+  write('You can see:'), nl,
+  list_things(Place),
+  write('You can go to:'), nl,
+  list_connections(Place).
